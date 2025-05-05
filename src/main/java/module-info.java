b556e9b384d48d535e993cefee38c81f7944fc55
TheHokester <jamiehokin@icloud.com>
2025-05-06 01:21:39 +1000

    Folder Refactoring

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module com.example.games {
+module com.hoke.games {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
@@ -9,14 +9,14 @@ module com.example.games {
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
-//    exports com.example.games;
-    opens com.example.games to javafx.fxml;
-    exports com.example.games.launcher;
-    opens com.example.games.launcher to javafx.fxml;
-    exports com.example.games.engine;
-    opens com.example.games.engine to javafx.fxml;
-    exports com.example.games.gameLib;
-    opens com.example.games.gameLib to javafx.fxml;
-    exports com.example.games.gameLib.chests;
-    opens com.example.games.gameLib.chests to javafx.fxml;
+//    exports com.hoke.games;
+//    opens   com.hoke.games to javafx.fxml;
+    exports com.hoke.games.launcher;
+    opens   com.hoke.games.launcher to javafx.fxml;
+    exports com.hoke.games.engine;
+    opens   com.hoke.games.engine to javafx.fxml;
+    exports com.hoke.games.gameLib;
+    opens   com.hoke.games.gameLib to javafx.fxml;
+    exports com.hoke.games.gameLib.chests;
+    opens   com.hoke.games.gameLib.chests to javafx.fxml;
 }
